7db00a8e5f577bc396d4e59e9cd1883fda829110
akhil-kancherla <92893236+akhil-kancherla@users.noreply.github.com>
2023-03-01 16:40:45 -0500

    Cypress Testing
    Added a cypress test for sign in button

--- FrontEnd/Karaokify/cypress/e2e/spec.cy.ts
+++ FrontEnd/Karaokify/cypress/e2e/spec.cy.ts
@@ -1,6 +1,11 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+describe('Home Screen', () => {
+  it('should login successfully', () => {
+    cy.visit('/'); // assumes the home screen URL is the root URL of the app
+
+    cy.get('[data-test=username-input]').type('testuser');
+    cy.get('[data-test=password-input]').type('testpassword');
+    cy.get('[data-test=login-button]').click();
+
+    cy.url().should('include', 'screen-search');
+  });
+});
