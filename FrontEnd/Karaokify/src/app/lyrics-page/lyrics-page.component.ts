b88d1156f460b8b1080131c5c366ac6946313992
akhil-kancherla <92893236+akhil-kancherla@users.noreply.github.com>
2023-03-28 15:53:23 -0400

    first commit

--- FrontEnd/Karaokify/src/app/lyrics-page/lyrics-page.component.ts
+++ FrontEnd/Karaokify/src/app/lyrics-page/lyrics-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { UsersService } from '../services/user.service';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lyrics-page',
@@ -10,22 +9,56 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class LyricsPageComponent implements OnInit {
 
-  str1:string="";
-  httpData : any[] = [];
-  safeUrl:any;
-  constructor(private http: HttpClient, private us:UsersService, private _san:DomSanitizer){}
+  youtubeUrl: string = "https://www.youtube.com/watch?v=S2Cti12XBw4";
+  videoId: string | undefined;
+  safeUrl: SafeResourceUrl | undefined;
+  lyrics: string | undefined;
+  loading: boolean = true;
+
+  constructor(
+    private http: HttpClient,
+    private _sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
-    this.us.currentMessage.subscribe(
-      (message) => (this.str1 = message)
-    );
+    this.videoId = this.getVideoId(this.youtubeUrl);
+    this.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + this.videoId);
 
-    this.safeUrl=this._san.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/Kq8zlXS2bUg");
-
-
-
+    this.loadLyrics();
   }
 
+  getVideoId(url: string) {
+    let videoId = '';
+    if (url.indexOf("v=") > -1) {
+      videoId = url.split("v=")[1];
+      let ampPos = videoId.indexOf("&");
+      if (ampPos > -1) {
+        videoId = videoId.substring(0, ampPos);
+      }
+    } else {
+      videoId = url.split("/").pop() ?? '';
+    }
+    return videoId;
+  }
 
+  stripHtmlTags(html: string) {
+    return html.replace(/<\/?[^>]+(>|$)/g, "");
+  }
 
+  loadLyrics() {
+    let url = `https://video.google.com/timedtext?lang=en&v=${this.videoId}`;
+    this.http.get(url, { responseType: 'text' }).subscribe((data) => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, "text/xml");
+      const textNodes = xmlDoc.getElementsByTagName('text');
+      let lyrics = '';
+      for (let i = 0; i < textNodes.length; i++) {
+        lyrics += textNodes[i].textContent + ' ';
+      }
+      this.lyrics = this.stripHtmlTags(lyrics);
+      this.loading = false;
+    });
+  }
 }
+
+
