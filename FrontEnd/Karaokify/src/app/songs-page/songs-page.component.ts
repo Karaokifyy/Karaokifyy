bde7a82ec01659257ebfdcf795ba9ad046893602
Skanda Kumaran <skandavskumaran@gmail.com>
2023-03-29 11:42:39 -0400

    Update songs-page.component.ts

--- FrontEnd/Karaokify/src/app/songs-page/songs-page.component.ts
+++ FrontEnd/Karaokify/src/app/songs-page/songs-page.component.ts
@@ -19,19 +19,22 @@ export class SongsPageComponent implements OnInit {
 
   str1:string="test";
   str2:string="test";
+  url:string= `http://localhost:8080/user/playlist/`;
   httpData : any[] = [];
   constructor(private http: HttpClient, private route: ActivatedRoute, private us:UsersService, private router: Router){}
 
   ngOnInit(){
-    const url = `http://localhost:8080/newSpotifySession`;
 
     this.us.currentMessage.subscribe(
       (message) => (this.str1 = message)
     );
 
+    this.url=this.url+this.str1;
+    console.log(this.url);
+
     this.route.queryParamMap
     .subscribe(params => {
-      this.http.post(url, JSON.stringify({playlistID:this.str1})).subscribe(data =>{
+      this.http.get(this.url).subscribe(data =>{
         const serverResult = JSON.parse(JSON.stringify(data))
         console.log(serverResult)
         const mapResult = Object.entries(serverResult)
